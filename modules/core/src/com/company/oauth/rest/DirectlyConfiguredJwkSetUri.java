da4291b1c482d67f7836a84796992c873d22cee9
Ilya Kuchmin <kuchmin@haulmont.com>
2021-07-02 16:50:44 +0400

    Add OAuth2Login configuration

--- modules/core/src/com/company/oauth/rest/DirectlyConfiguredJwkSetUri.java
+++ modules/core/src/com/company/oauth/rest/DirectlyConfiguredJwkSetUri.java
@@ -2,19 +2,29 @@ package com.company.oauth.rest;
 
 import com.company.oauth.rest.auth.CubaUserJwtAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class DirectlyConfiguredJwkSetUri extends WebSecurityConfigurerAdapter {
 
     @Inject
@@ -33,8 +43,12 @@ public class DirectlyConfiguredJwkSetUri extends WebSecurityConfigurerAdapter {
         http.authenticationProvider(cubaUserJwtAuthenticationProvider);
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
+                .anyRequest().authenticated().and()
+//                .oauth2Login()
+//                .loginPage("/rest/login/oauth2")
+//                .clientRegistrationRepository(clientRegistrationRepository())
+//                .authorizedClientService(authorizedClientService()).and()
+//                .oauth2Client().and()
                 .oauth2ResourceServer().jwt()
 //                .authenticationManager(http.getSharedObject(AuthenticationManager.class))
                 .decoder(jwtDecoder)
@@ -42,5 +56,25 @@ public class DirectlyConfiguredJwkSetUri extends WebSecurityConfigurerAdapter {
 //        .and().and()
 //                .addFilter(new OAuth2ClientAuthenticationProcessingFilter())
         ;
+    }
+
+    private static List<String> clients = Arrays.asList("google", "facebook");
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(
+                clientRegistrationRepository());
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+    }
+
+    private ClientRegistration keycloakClientRegistration() {
+        return ClientRegistration.withRegistrationId("keycloak")
+                .clientId("marketplace")
+                .clientSecret("c1ea8004-1cae-4b80-949e-75769e90218b")
+                .build();
     }
 }
---
+++ modules/core/src/com/company/oauth/rest/OAuth2LoginConfig.java
@@ -0,0 +1,69 @@
+package com.company.oauth.rest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class OAuth2LoginConfig {
+
+    @EnableWebSecurity
+    public static class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest().authenticated().and()
+                    .oauth2Login().authorizationEndpoint().baseUri("/rest/oauth2/authorization").and()
+                                  .redirectionEndpoint().baseUri("/rest/login/oauth2/code/*");
+        }
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService(
+            ClientRegistrationRepository clientRegistrationRepository) {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientRepository authorizedClientRepository(
+            OAuth2AuthorizedClientService authorizedClientService) {
+        return new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(authorizedClientService);
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("marketplace")
+                .clientId("marketplace")
+                .clientSecret("ef3e4e98-1e90-4811-948f-c38c22a73a54")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUriTemplate("{baseUrl}/rest/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email", "address", "phone")
+                .authorizationUri("http://bespoke-hse2.haulmont.com/app/realms/master/protocol/openid-connect/auth")
+                .tokenUri("http://bespoke-hse2.haulmont.com/app/realms/master/protocol/openid-connect/token")
+                .userInfoUri("http://bespoke-hse2.haulmont.com/app/realms/master/protocol/openid-connect/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("http://bespoke-hse2.haulmont.com/app/realms/master/protocol/openid-connect/certs")
+                .clientName("Marketplace")
+                .build();
+    }
+}
